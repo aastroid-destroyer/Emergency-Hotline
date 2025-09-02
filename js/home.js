96e66e53ff8524d53b4b41fb89c654f9d819ec45
aastroid-destroyer <abraraqib33@gmail.com>
2025-09-02 17:30:15 +0600

    calling function

--- js/home.js
+++ js/home.js
@@ -1,14 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
+document.addEventListener('DOMContentLoaded', function () {
+
     const heartCountElement = document.getElementById('heart-count');
-    
-    
+
+
     const heartIcons = document.getElementsByClassName('heart-icon');
-    
- 
+
+
     for (let i = 0; i < heartIcons.length; i++) {
-        heartIcons[i].addEventListener('click', function() {
+        heartIcons[i].addEventListener('click', function () {
             heartCountElement.textContent++;
         });
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const callButtons = document.querySelectorAll('.btn-call');
+
+
+    const coinCountElement = document.getElementById('coin-count');
+
+    let coinCount = parseInt(coinCountElement.textContent);
+
+    callButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+
+            const serviceName = button.getAttribute('data-service');
+            const emergencyNumber = button.getAttribute('data-number');
+
+            if (coinCount >= 20) {
+
+                coinCount -= 20;
+                coinCountElement.textContent = coinCount;
+                alert(`Calling ${serviceName}: ${emergencyNumber}`);
+
+            } else {
+
+                alert('You do not have sufficient coins to call.');
+            }
+        });
+    });
+});
+
+
+
